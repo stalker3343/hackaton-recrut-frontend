06b35ca063be81e0fb56792955067eff93066e5d
Artem Byastic <stalker3343@gmail.com>
2023-10-22 08:26:59 +0300

    for more safe

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
   target: 'static',
+  generate: {
+    fallback: true,
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - center-invest-dock',
